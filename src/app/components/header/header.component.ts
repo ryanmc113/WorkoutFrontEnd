d776b37af1b1a4ed41b52efe34b43c070fd68588
Ryan McUmber <ryanmma.rm@gmail.com>
2025-01-14 07:59:26 -0500

    fixed the login bug and dashboard route works

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,30 +1,41 @@
-import { Component,Injectable, PLATFORM_ID, Inject  } from '@angular/core';
+import { Component,Injectable, PLATFORM_ID, Inject, OnInit  } from '@angular/core';
 import { CommonModule,isPlatformBrowser } from '@angular/common';
 import { LoginServiceService } from '../../services/login-service.service';
+import { RouterModule } from '@angular/router';
+import { AuthServiceService } from '../../services/auth-service.service';
+import { BehaviorSubject } from 'rxjs';
+
 
 
 
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-   userLoggedIn = false;
+export class HeaderComponent implements OnInit {
+  public userLoggedIn = false; 
+  public userSessionLoggedIn = false;
 
    constructor(@Inject(PLATFORM_ID) private platformId: Object, 
-                private loginService: LoginServiceService) {
+                private loginService: LoginServiceService, private authService: AuthServiceService) {
                   this.loginService.isLoggedIn.subscribe(loggedIn => {
                     this.userLoggedIn = loggedIn;
+                    this.userSessionLoggedIn = loggedIn;
                   });    
+                  // console.log(localStorage.getItem('expiresIn'))
   }
 
-  // ngOnInit() {
-  //   if (isPlatformBrowser(this.platformId)) {
-  //     this.userLoggedIn = this.authService.isLoggedIn();
-  //   }
-  // }
+  ngOnInit(): void {
+    if (typeof window !== 'undefined') {
+      this.userSessionLoggedIn = sessionStorage.getItem('expiresIn') ? true : false;
+      console.log(localStorage.getItem('expiresIn'))
+    }
+  }
+
+  
+
 
 }
