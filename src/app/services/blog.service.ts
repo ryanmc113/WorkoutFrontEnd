4f47c95e8a541c22b26de9ac531e70c9ec5f3a35
Ryan McUmber <ryanmma.rm@gmail.com>
2025-03-13 14:10:32 -0400

    update to blog

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { tap, shareReplay } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie-service';
+import { cp } from 'fs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +10,36 @@ import { tap, shareReplay } from 'rxjs/operators';
 export class BlogService {
   private url: string;
 
-  constructor(private http: HttpClient) { 
-    this.url = 'http://localhost:8080/workout';
+  constructor(private http: HttpClient, private cookieService: CookieService) { 
+    this.url = 'http://localhost:8080';
+  }
+
+  getBlogs() {
+    // const token = localStorage.getItem('token');
+    const token = this.cookieService.get('jwt_token'); 
+    if (!token) {
+      throw new Error('No auth token found!');
+    }
+
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'  
+    });
+    
+    return this.http.get<any>(this.url+'/blog/list', { headers })
+      .pipe(
+        tap(res => {
+          console.log(res);
+        }),
+        shareReplay()
+      );
   }
 
   saveBlog(blogForm: any) {
     console.log('Blog saved:', blogForm);
 
-    const token = localStorage.getItem('token');
+    // const token = localStorage.getItem('token');
+    const token = this.cookieService.get('jwt_token');
     if (!token) {
       throw new Error('No auth token found!');
     }
--- src/app/services/create-blog.service.ts
+++ src/app/services/create-blog.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { tap, shareReplay } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +9,14 @@ import { tap, shareReplay } from 'rxjs/operators';
 export class CreateBlogService {
   private url: string;
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient, private cookieService: CookieService) { 
       this.url = 'http://localhost:8080/blog';
     }
   
     saveBlog(blogForm: any) {
       console.log('Blog saved:', blogForm);
   
-      const token = localStorage.getItem('token');
+      const token = this.cookieService.get('jwt_token');
       if (!token) {
         throw new Error('No auth token found!');
       }
